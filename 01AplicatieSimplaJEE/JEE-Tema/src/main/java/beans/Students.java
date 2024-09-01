3fa982c370991b069b85f8ae515acc034fd8c310
DarianXD <laduncadarian@gmail.com>
2024-09-01 18:15:19 +0300

    01 Adaugare clasa Studenti pentru a gestiona o lista de StudentiBean

---
+++ 01AplicatieSimplaJEE/JEE-Tema/src/main/java/beans/Students.java
@@ -0,0 +1,18 @@
+package beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Students {
+    private List<StudentBean> studentList = new ArrayList<>();
+
+    public List<StudentBean> getStudentList()
+    {
+        return studentList;
+    }
+
+    public void setStudentList(List<StudentBean> studentList)
+    {
+        this.studentList = studentList;
+    }
+}
